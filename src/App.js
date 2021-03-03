1c164dedac81675adc22ef093d26124e83fae92c
FernandoQ <octavius.sys@gmail.com>
2021-03-02 19:33:19 -0500

    agregando Lazy en el componente App especificament a las rutas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Route } from "wouter";
 import {GifsProvider} from 'context/contextGifs'
-import GifDetails from "pages/gifDetails";
 import Home from "pages/home";
-import SearchGifs from "pages/searchGifs";
+
+const SearchGifsLazy=React.lazy(()=>import('pages/searchGifs'))
+const GifDetailsLazy=React.lazy(()=>import('pages/gifDetails'))
 const App = () => {
   return (
     <GifsProvider >
+      <Suspense fallback={null}>
       <Route path="/" component={Home} />
-      <Route path="/gifs/:keyword" component={SearchGifs} />
-      <Route path="/gif/:id" component={GifDetails} />
+      <Route path="/gifs/:keyword" component={SearchGifsLazy} />
+      <Route path="/gif/:id" component={GifDetailsLazy} />
+      </Suspense>
     </GifsProvider>
   );
 };
